700692f62c926d5b6bda22682d188a83397a3d4b
juliobellano <87596624+juliobellano@users.noreply.github.com>
2024-06-11 19:13:54 +0800

    Update fetch_stock.php

--- fetch_stock.php
+++ fetch_stock.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['query'])) {
     $query = $_GET['query'];
-    $api_key = "EoORQU1v7jsIphhGyFHr3opEsSkHGkqX";
+    $api_key ="1U3BHsFpewhF_TQLOMop5WHAmrtCEubs";
     $api_url = "https://api.polygon.io/v3/reference/tickers?search=$query&active=true&apiKey=$api_key";
     $api_response = file_get_contents($api_url);
     echo $api_response;
